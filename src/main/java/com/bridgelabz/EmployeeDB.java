a719e1e40905ba77a8834ec1ac17d190c5687cc9
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-06-16 02:30:27 +0530

    Added: Adding Number, Address and Department in table.

--- src/main/java/com/bridgelabz/EmployeeDB.java
+++ src/main/java/com/bridgelabz/EmployeeDB.java
@@ -20,17 +20,22 @@ public class EmployeeDB {
             con = DriverManager.getConnection(jdbcURL, userName, password);
             System.out.println("Connection is successfully" + con);
             Statement statement = con.createStatement();
-            statement.executeUpdate("insert into employee(name,salary,age,department,joining_date) values ('Ritesh',40000,22,'IT Engineer','2021-04-18')");
-            statement.executeUpdate("update employee set Age=61 where Emp_Id in (2)");
-            statement.executeUpdate("alter table employee modify column salary double");
-            statement.executeQuery("desc employee");
-            statement.execute("insert into employee(name,salary,age,department,joining_date) values ('Amit',50000,24,'IT Engineer','2022-02-15'), ('Rohan',40000,26,'Software Engineer','2018-03-14'), ('Suraj',45000,28,'IT Engineer','2019-05-10'), ('Payal',55000,27,'Software Engineer','2020-08-09'), ('Mohak',25000,30,'IT Engineer','2016-06-26');");
+//            statement.executeUpdate("insert into employee(name,salary,age,department,joining_date) values ('Ritesh',40000,22,'IT Engineer','2021-04-18')");
+//            statement.executeUpdate("update employee set Age=61 where Emp_Id in (2)");
+//            statement.executeUpdate("alter table employee modify column salary double");
+//            statement.executeQuery("desc employee");
+//            statement.execute("insert into employee(name,salary,age,department,joining_date) values ('Amit',50000,24,'IT Engineer','2022-02-15'), ('Rohan',40000,26,'Software Engineer','2018-03-14'), ('Suraj',45000,28,'IT Engineer','2019-05-10'), ('Payal',55000,27,'Software Engineer','2020-08-09'), ('Mohak',25000,30,'IT Engineer','2016-06-26');");
+//
+//            PreparedStatement preparedStatement=con.prepareStatement("alter table employee add column " +
+//                    "gender varchar(20)");
+//            ResultSet resultSet = statement.executeQuery("select Gender,sum(salary) as sum," +
+//                    "avg(salary) as average,max(salary) as maximum,min(salary) as minimum" +
+//                    " from employee group by Gender");
 
-            PreparedStatement preparedStatement=con.prepareStatement("alter table employee add column " +
-                    "gender varchar(20)");
-            ResultSet resultSet = statement.executeQuery("select Gender,sum(salary) as sum," +
-                    "avg(salary) as average,max(salary) as maximum,min(salary) as minimum" +
-                    " from employee group by Gender");
+            statement.execute("alter table employee add column phone varchar(30)");
+            statement.execute("alter table employee add column address varchar(90)");
+            statement.execute("update employee set department = not null");
+            ResultSet resultSet =statement.executeQuery("select * from employee");
 
             while (resultSet.next()) {
                 System.out.println("Id: " + resultSet.getInt("Emp_Id"));
